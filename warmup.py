7d3f229e32b3655fe00243d268024c473d5ba2ab
Alberto Puentes <albertopuentes00@gmail.com>
2021-04-09 12:16:45 -0500

    Warmup Exercises

--- warmup.py
+++ warmup.py
@@ -1,3 +1,5 @@
+# 1
+
 input_truck = "toyota tacoma"
 input_truck.split()
 
@@ -6,6 +8,23 @@ model = input_truck.split()[-1]
 
 truck = dict(make = make, model = model)
 print(truck)
+
+# 2
+
+input_truck = "toyota tacoma"
+input_truck.split()
+
+make = input_truck.split()[0]
+model = input_truck.split()[-1]
+
+truck = dict(make = make, model = model)
+
+truck['make'] = truck['make'].capitalize()
+truck['model'] = truck['model'].capitalize()
+
+print(truck)
+
+# 3 
 
 
 
