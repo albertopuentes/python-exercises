5184f839bfeae587af14c9fca6b2ebcfa7ac51f7
Alberto Puentes <albertopuentes00@gmail.com>
2021-04-13 14:53:27 -0500

    Import Exercises Python

--- import_exercises.py
+++ import_exercises.py
@@ -9,5 +9,15 @@ from function_exercises import calculate_tip
 
 print(calculate_tip(.15, 100))
 
+from itertools import product
+# 2.a.
+print(len(list(product('abc', '123'))))
 
+# 2.b.
+from itertools import combinations
+print(len(list(combinations('abcd', 2))))
+
+#2.c. 
+from itertools import permutations
+print(len(list(permutations('abcd', 2))))
 
