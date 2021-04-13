e875a62772960c025a63487401a6f842ec5cc28c
Alberto Puentes <albertopuentes00@gmail.com>
2021-04-13 14:23:40 -0500

    Import Exercises Python

---
+++ import_exercises.py
@@ -0,0 +1,13 @@
+import function_exercises
+
+ 
+print(function_exercises.is_vowel('a'))
+print(function_exercises.apply_discount(100, .25))
+print(function_exercises.remove_vowels('testme'))
+
+from function_exercises import calculate_tip
+
+print(calculate_tip(.15, 100))
+
+
+
